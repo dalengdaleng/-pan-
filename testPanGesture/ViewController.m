d305eb49ea20e55ff6a67cf2da8283c13c13703e
ju <mike.hu@justalk.com>
2017-03-20 09:19:44 +0800

    first

--- testPanGesture/ViewController.m
+++ testPanGesture/ViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "UIView+XMLPanGesture.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<XMLPanGestureDelegate>
+@property(nonatomic, strong) UIView *testView;
 @end
 
 @implementation ViewController
@@ -17,6 +18,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIView *tempView = [[UIView alloc] initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height - 100, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height+100)];
+    tempView.backgroundColor = [UIColor redColor];
+    self.testView = tempView;
+    [self.view addSubview:tempView];
+    
+    [self.testView addPanGestureWithEndFrame:CGRectMake(0, 0, CGRectGetWidth([UIScreen mainScreen].bounds), self.testView.frame.size.height)];
+    self.testView.panGestureDelegate = self;
 }
 
 
@@ -25,5 +33,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - XMLPanGestureDelegate
+
+- (void)xmlPanGesture:(UIPanGestureRecognizer *)panGesture didChangeWithView:(UIView *)targetView {
+    if (panGesture.state == UIGestureRecognizerStateBegan) {
+        self.testView.backgroundColor = [UIColor orangeColor];
+    }
+}
+
+- (void)xmlPanGesture:(UIPanGestureRecognizer *)panGesture willShowTargetView:(UIView *)targetView {
+    self.testView.backgroundColor = [UIColor orangeColor];
+    
+}
+
+- (void)xmlPanGesture:(UIPanGestureRecognizer *)panGesture didShowTargetView:(UIView *)targetView {
+    
+}
+
+- (void)xmlPanGesture:(UIPanGestureRecognizer *)panGesture willHideTargetView:(UIView *)targetView {
+    
+}
+
+- (void)xmlPanGesture:(UIPanGestureRecognizer *)panGesture didHideTargetView:(UIView *)targetView {
+//    self.testView.backgroundColor = [UIColor redColor];
+}
 
 @end
